5ca436e04318b62421cf8cf394e016d91407c3bc
Qiyuan Shi <qshi6@jhu.edu>
2026-02-03 12:37:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="rgrg",
     version="1.0",
-    packages=find_packages("src"),
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
